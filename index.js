796fc7d6e48518300dc05a4e555827fd2d5b4aca
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-21 16:32:09 +0200

    Provide a reusable constructor

--- index.js
+++ index.js
@@ -8,6 +8,53 @@ var through = require('through2');
 /**
  * optipng imagemin plugin
  *
+ * @param {Object} file
+ * @param {String} enc
+ * @param {Object} opts
+ * @param {Function} cb
+ * @api private
+ */
+
+function plugin(file, enc, opts, cb) {
+	if (file.isNull()) {
+		cb(null, file);
+		return;
+	}
+
+	if (file.isStream()) {
+		cb(new Error('Streaming is not supported'));
+		return;
+	}
+
+	if (!isPng(file.contents)) {
+		cb(null, file);
+		return;
+	}
+
+	var exec = new ExecBuffer();
+	var args = ['-strip', 'all', '-quiet', '-clobber'];
+	var optimizationLevel = opts.optimizationLevel || 3;
+
+	if (typeof optimizationLevel === 'number') {
+		args.push('-o', optimizationLevel);
+	}
+
+	exec
+		.use(optipng, args.concat(['-out', exec.dest(), exec.src()]))
+		.run(file.contents, function (err, buf) {
+			if (err) {
+				cb(err);
+				return;
+			}
+
+			file.contents = buf;
+			cb(null, file);
+		});
+}
+
+/**
+ * Module exports
+ *
  * @param {Object} opts
  * @api public
  */
@@ -16,39 +63,21 @@ module.exports = function (opts) {
 	opts = opts || {};
 
 	return through.obj(function (file, enc, cb) {
-		if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
+		plugin(file, enc, opts, cb);
+	});
+};
 
-		if (file.isStream()) {
-			cb(new Error('Streaming is not supported'));
-			return;
-		}
+/**
+ * Module exports constructor
+ *
+ * @param {Object} opts
+ * @api public
+ */
 
-		if (!isPng(file.contents)) {
-			cb(null, file);
-			return;
-		}
+module.exports.ctor = function (opts) {
+	opts = opts || {};
 
-		var exec = new ExecBuffer();
-		var args = ['-strip', 'all', '-quiet', '-clobber'];
-		var optimizationLevel = opts.optimizationLevel || 3;
-
-		if (typeof optimizationLevel === 'number') {
-			args.push('-o', optimizationLevel);
-		}
-
-		exec
-			.use(optipng, args.concat(['-out', exec.dest(), exec.src()]))
-			.run(file.contents, function (err, buf) {
-				if (err) {
-					cb(err);
-					return;
-				}
-
-				file.contents = buf;
-				cb(null, file);
-			});
+	return through.ctor({ objectMode: true }, function (file, enc, cb) {
+		plugin(file, enc, opts, cb);
 	});
 };
--- test/test.js
+++ test/test.js
@@ -26,3 +26,25 @@ test('optimize a PNG', function (t) {
 		stream.end(file);
 	});
 });
+
+test('optimize a PNG using ctor', function (t) {
+	t.plan(3);
+
+	var Optipng = optipng.ctor();
+
+	fs.readFile(path.join(__dirname, 'fixtures/test.png'), function (err, buf) {
+		t.assert(!err);
+
+		var stream = new Optipng();
+		var file = new File({
+			contents: buf
+		});
+
+		stream.on('data', function (data) {
+			t.assert(data.contents.length < buf.length);
+			t.assert(isPng(data.contents));
+		});
+
+		stream.end(file);
+	});
+});
