aaa7dc070d86e9e466ee4368fc71cac000f53120
Kevin Martensson <kevinmartensson@gmail.com>
2015-01-20 20:36:10 +0100

    Only use optimized file if it's smaller than source

--- index.js
+++ index.js
@@ -47,7 +47,10 @@ module.exports = function (opts) {
 					return;
 				}
 
-				file.contents = buf;
+				if (buf.length < file.contents.length) {
+					file.contents = buf;
+				}
+
 				cb(null, file);
 			});
 	});
