9140df1b0ce805b9f096f22ab3d4132f8fa77aa9
Kevin Martensson <kevinmartensson@gmail.com>
2017-06-15 17:19:00 +0200

    Meta tweaks

--- test.js
+++ test.js
@@ -1,42 +1,29 @@
 import fs from 'fs';
 import path from 'path';
 import isPng from 'is-png';
-import pify from 'pify';
 import test from 'ava';
 import m from './';
 
-const fsP = pify(fs);
-
-let buf;
-let corrupt;
-
-test.before(async () => {
-	buf = await fsP.readFile(path.join(__dirname, 'fixture.png'));
-	corrupt = await fsP.readFile(path.join(__dirname, 'fixture-corrupt.png'));
-});
+const fixture = fs.readFileSync(path.join(__dirname, 'fixture.png'));
 
 test('optimize a PNG', async t => {
-	const data = await m()(buf);
-	t.true(data.length < buf.length);
+	const data = await m()(fixture);
+	t.true(data.length < fixture.length);
 	t.true(isPng(data));
 });
 
-test('throw on empty input', t => {
-	t.throws(m()(), /Expected a buffer/);
-});
-
-test('throw on corrupt image', t => {
-	t.throws(m()(corrupt), /PNG file appears to be corrupted by text file conversions/);
+test('throw on empty input', async t => {
+	await t.throws(m()(), /Expected a buffer/);
 });
 
 test('bitDepthReduction', async t => {
-	t.notThrows(m({bitDepthReduction: true})(buf));
+	await t.notThrows(m({bitDepthReduction: true})(fixture));
 });
 
 test('colorTypeReduction', async t => {
-	t.notThrows(m({colorTypeReduction: true})(buf));
+	await t.notThrows(m({colorTypeReduction: true})(fixture));
 });
 
 test('paletteReduction', async t => {
-	t.notThrows(m({paletteReduction: true})(buf));
+	await t.notThrows(m({paletteReduction: true})(fixture));
 });
